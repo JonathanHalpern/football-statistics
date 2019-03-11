84d9dae3de4cb23792a28309b2c95d67a447f83c
Jonathan Halpern <jonnyhalpern@gmail.com>
2019-03-11 16:40:11 +0700

    added filtering logic

--- src/Pages/Players.tsx
+++ src/Pages/Players.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { RouteComponentProps } from "@reach/router";
-import { Spin } from "antd";
+import styled from "@emotion/styled";
+
+import { Spin, Form, Select } from "antd";
+
 import {
   usePlayers,
   useOnNewPage,
@@ -10,15 +13,75 @@ import {
 
 import { Players } from "../Containers";
 
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+const StyledForm = styled(Form)`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+//TODO: refactor filtering form into separate component and add filters for age and value
+
 const PlayersPage: FC<RouteComponentProps> = () => {
-  const players = usePlayers();
+  const [nationalityFilter, updateNationalityFilter] = useState("");
+  const [teamFilter, updateTeamFilter] = useState("");
+  let players = usePlayers();
   useOnNewPage("Players");
   const isLoadingPlayers = useLoadingPlayers();
   const isLoadingTeams = useLoadingTeams();
+  const nationalities = [...new Set(players.map(player => player.nationality))];
+  const teamNames = [...new Set(players.map(player => player.team_name))];
+
+  if (nationalityFilter) {
+    players = players.filter(
+      player => player.nationality === nationalityFilter
+    );
+  }
+  if (teamFilter) {
+    players = players.filter(player => player.team_name === teamFilter);
+  }
+
   if (isLoadingPlayers || isLoadingTeams) return <Spin size="large" />;
   if (!players) return <p>No players</p>;
 
-  return <Players tableBodyData={players} />;
+  return (
+    <>
+      <StyledForm>
+        <FormItem label="Team">
+          <Select
+            value={teamFilter}
+            style={{ width: 200 }}
+            onChange={newId => {
+              updateTeamFilter(newId);
+            }}
+          >
+            {teamNames.map(value => (
+              <Option value={value} key={value}>
+                {value}
+              </Option>
+            ))}
+          </Select>
+        </FormItem>
+        <FormItem label="Nationality">
+          <Select
+            value={nationalityFilter}
+            style={{ width: 200 }}
+            onChange={newId => {
+              updateNationalityFilter(newId);
+            }}
+          >
+            {nationalities.map(value => (
+              <Option value={value} key={value}>
+                {value}
+              </Option>
+            ))}
+          </Select>
+        </FormItem>
+      </StyledForm>
+      <Players tableBodyData={players} />
+    </>
+  );
 };
 
 export default PlayersPage;
